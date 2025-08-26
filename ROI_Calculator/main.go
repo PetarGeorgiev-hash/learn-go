3925f77a22b38131e21bf459700b57a46390c325
Petar Georgiev <petiyo07@abv.bg>
2025-08-26 16:31:26 +0300

    commit

---
+++ ROI_Calculator/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 6.5
+	var investmentAmount, years float64
+	expectedReturnRate := 5.5
+
+	fmt.Print("Investment amout: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Exptected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+}
