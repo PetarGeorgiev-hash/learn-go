66c50793659be6506793bf954b4c0b2d6851b0e8
Petar Georgiev <petiyo07@abv.bg>
2025-08-28 01:00:01 +0300

    split code with packages

--- banking/main.go
+++ banking/main.go
@@ -1,47 +1,25 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"strconv"
+
+	"github.com/PetarGeorgiev-hash/learning-go/file"
 )
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() (float64, error) {
-	balance, err := os.ReadFile(accountBalanceFile)
-	if err != nil {
-		return 1000.0, errors.New("failed to find balance")
-	}
-	balanceText := string(balance)
-	floatBalance, err := strconv.ParseFloat(balanceText, 64)
-	if err != nil {
-		return 1000.0, errors.New("failed to parse balance")
-	}
-	return floatBalance, nil
-}
-
-func writeToFile(balance float64) {
-	balanceText := fmt.Sprint(balance)
-	os.WriteFile(accountBalanceFile, []byte(balanceText), 0664)
-}
-
 func main() {
 	fmt.Println("Welcome to GO Bank")
-	accBalance, err := getBalanceFromFile()
+	accBalance, err := file.GetFloatFromFile(accountBalanceFile)
 	if err != nil {
 		fmt.Println("ERROR")
 		fmt.Println(err)
 		fmt.Println("-------")
-		panic("Can't continue sorry")
+		fmt.Println("Can't continue sorry")
 	}
+	presentOptions()
+
 	for {
-		fmt.Println("What do you wish to do ?")
-		fmt.Println("1. Check balance")
-		fmt.Println("2. Deposit money")
-		fmt.Println("3. Withdraw money")
-		fmt.Println("4. Exit")
 
 		fmt.Print("Your choice: ")
 		var choice int
@@ -61,7 +39,7 @@ func main() {
 			}
 			accBalance += depositAmonut
 			fmt.Println("Account balance updated: ", accBalance)
-			writeToFile(accBalance)
+			file.WriteFloatToFile(accBalance, accountBalanceFile)
 		case 3:
 			fmt.Print("Withdraw amaunt: ")
 			var withdrawAmount float64
@@ -74,7 +52,7 @@ func main() {
 
 			accBalance -= withdrawAmount
 			fmt.Println("Account balance updated: ", accBalance)
-			writeToFile(accBalance)
+			file.WriteFloatToFile(accBalance, accountBalanceFile)
 		default:
 			fmt.Println("Goodbye!")
 			return
