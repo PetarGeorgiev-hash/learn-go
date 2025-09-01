07794483d655332f4ac002401a4552c16a1340d6
Petar Georgiev <petiyo07@abv.bg>
2025-09-01 20:05:40 +0300

    maps

---
+++ lists/maps.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{"Amazon Web Services": "https://aws.com",
+		"Google": "https://gcp.com"}
+
+	fmt.Println(websites["Google"])
+}
